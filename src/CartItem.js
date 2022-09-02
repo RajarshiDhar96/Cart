cf7222aab0e5292f7f20b1426bb381643651c11d
Rohan Dhar <rajarshid920@gmail.com>
2022-09-02 13:09:04 +0530

    using state in React

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,11 +1,27 @@
 import React from 'react';
 //for a class component to be react component we have to give a render method
 class CartItem extends React.Component{
+    //state is a built-in React object that is used to contain data or information about the component
+    constructor()
+    {
+        super();
+        this.state={
+            price:1000,
+            title:'Mobile Phone',
+            qty:1,
+            img:''
+        }
+    }
+
     render(){
+
+        const{price,title,qty}=this.state;
+
+
 
         return(
             //JSX code
-          <div className='cart-item'>
+        <div className='cart-item'>
 
             <div className='left-block'>
 
@@ -14,16 +30,22 @@ class CartItem extends React.Component{
             </div>
 
             <div className='right-block'>
-                <div style={{fontSize:25}}>Phone</div>
-                <div style={{color:'#777'}}>Rs 999</div>
-                <div style={{color:'#777'}}> Qty: 1</div>
-            </div>
+                <div style={{fontSize:25}}>{title}</div>
+                <div style={{color:'#777'}}>Rs {price}</div>
+                <div style={{color:'#777'}}> Qty: {qty}</div>
+            
+            
+                <div className='cart-item-actions'>
 
-            <div className='cart-item-actions'>
+                    <img alt='increase' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/4315/4315609.png'/>
+                    <img alt='decrease' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/7080/7080604.png'/>
+                    <img alt='delete' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/3221/3221897.png'/>
 
-            </div>
+                </div>
 
           </div>
+
+        </div>
         )
 
     }
